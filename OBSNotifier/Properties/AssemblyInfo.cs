cfd7301fb819ed11f267c9193edb3071c3fb1686
Дмитрий Сальников <salnikov.mine@yandex.ru>
2022-11-07 16:32:50 +0300

    bump version to 1.2.0

--- OBSNotifier/Properties/AssemblyInfo.cs
+++ OBSNotifier/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("1.1.1")]
-[assembly: AssemblyFileVersion("1.1.1")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
