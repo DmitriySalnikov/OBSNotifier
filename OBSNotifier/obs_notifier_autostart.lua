3c1e686977819c2bb8939c9929318d2c330f4d76
Дмитрий Сальников <salnikov.mine@yandex.ru>
2022-11-09 18:18:40 +0300

    added support for UTF-8 path to the program file in the autorun script

--- OBSNotifier/obs_notifier_autostart.lua
+++ OBSNotifier/obs_notifier_autostart.lua
@@ -13,18 +13,23 @@ local BELOW_NORMAL_PRIORITY_CLASS = 0x00004000
 local NORMAL_PRIORITY_CLASS = 0x00000020
 local CREATE_NEW_PROCESS_GROUP = 0x00000200
 
-ffi.cdef[[ typedef struct _STARTUPINFOA { uint32_t cb; void * lpReserved; void * lpDesktop; void * lpTitle; uint32_t dwX; uint32_t dwY; uint32_t dwXSize; uint32_t dwYSize; uint32_t dwXCountChars; uint32_t dwYCountChars; uint32_t dwFillAttribute; uint32_t dwFlags; uint16_t wShowWindow; uint16_t cbReserved2; void * lpReserved2; void ** hStdInput; void ** hStdOutput; void ** hStdError; } STARTUPINFOA, *LPSTARTUPINFOA; typedef struct _PROCESS_INFORMATION { void ** hProcess; void ** hThread; uint32_t dwProcessId; uint32_t dwThreadId; } PROCESS_INFORMATION, *LPPROCESS_INFORMATION; uint32_t CreateProcessA( void *, const char * commandLine, void *, void *, uint32_t, uint32_t, void *, const char * currentDirectory, LPSTARTUPINFOA, LPPROCESS_INFORMATION ); uint32_t CloseHandle(void **);]]
+ffi.cdef [[ typedef struct _STARTUPINFOW { uint32_t cb; void * lpReserved; void * lpDesktop; void * lpTitle; uint32_t dwX; uint32_t dwY; uint32_t dwXSize; uint32_t dwYSize; uint32_t dwXCountChars; uint32_t dwYCountChars; uint32_t dwFillAttribute; uint32_t dwFlags; uint16_t wShowWindow; uint16_t cbReserved2; void * lpReserved2; void ** hStdInput; void ** hStdOutput; void ** hStdError; } STARTUPINFOW, *LPSTARTUPINFOW; typedef struct _PROCESS_INFORMATION { void ** hProcess; void ** hThread; uint32_t dwProcessId; uint32_t dwThreadId; } PROCESS_INFORMATION, *LPPROCESS_INFORMATION; uint32_t CreateProcessW( void *, const wchar_t * commandLine, void *, void *, uint32_t, uint32_t, void *, const wchar_t *, LPSTARTUPINFOW, LPPROCESS_INFORMATION ); uint32_t CloseHandle(void **);]]
+ffi.cdef [[ int MultiByteToWideChar(unsigned int CodePage, unsigned int dwFlags, const char* lpMultiByteStr, int cbMultiByte, wchar_t* lpWideCharStr, int cchWideChar); ]]
 
-local function execute(commandLine, priority, currentDirectory)
-   local si = ffi.new"STARTUPINFOA"
-   si.cb = ffi.sizeof(si)
-   local pi = ffi.new"PROCESS_INFORMATION"
-   local ok = ffi.C.CreateProcessA(nil, commandLine, nil, nil, 0, bit32.bor(priority, CREATE_NEW_PROCESS_GROUP), nil, currentDirectory, si, pi) ~= 0
-   if ok then
-      ffi.C.CloseHandle(pi.hProcess)
-      ffi.C.CloseHandle(pi.hThread)
-   end
-   return ok  -- true/false
+local function execute(commandLine, priority)
+	local si = ffi.new("STARTUPINFOW")
+	si.cb = ffi.sizeof(si)
+	local pi = ffi.new("PROCESS_INFORMATION")
+
+	local widestr = ffi.new("wchar_t[?]", 1024);
+	ffi.C.MultiByteToWideChar(65001, 0, commandLine, #commandLine, widestr, 1024)
+
+	local ok = ffi.C.CreateProcessW(nil, widestr, nil, nil, 0, bit32.bor(priority, CREATE_NEW_PROCESS_GROUP), nil, nil, si, pi) ~= 0
+	if ok then
+		ffi.C.CloseHandle(pi.hProcess)
+		ffi.C.CloseHandle(pi.hThread)
+	end
+	return ok -- true/false
 end
 -- ~WinAPI
 
@@ -45,13 +50,14 @@ end
 
 function script_update(settings)
 	local lowprio = obs.obs_data_get_bool(settings, "lowprio")
-	start_OBSNotifier(path, lowprio)
+	start_OBSNotifier(lowprio)
 end
 
-function start_OBSNotifier(path, lowprio)
+function start_OBSNotifier(lowprio)
 	local prio = lowprio and BELOW_NORMAL_PRIORITY_CLASS or NORMAL_PRIORITY_CLASS
 	local StartCommand = "\"&OBS_NOTIFIER_PATH&\" --force_close"
-	obs.script_log(obs.LOG_INFO, "Executing command: " .. StartCommand .. ". With " .. (lowprio and "\"Below Normal\"" or "\"Normal\"") .. " priority.")
+	obs.script_log(obs.LOG_INFO,
+		"Executing command: " .. StartCommand .. ". With " .. (lowprio and "\"Below Normal\"" or "\"Normal\"") .. " priority.")
 
 	local output = execute(StartCommand, prio)
 	obs.script_log(obs.LOG_INFO, "Output: " .. tostring(output))
